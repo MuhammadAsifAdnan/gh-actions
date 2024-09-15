887fdd6c12e19ef92f773323d09ae371107a0d25
Asif Adnan <asif.adnan.cse@gmail.com>
2024-09-15 09:04:02 +0600

    intentional lint error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const title = "Welcome to Next.js!";
   return (
     <div className={styles.page}>
       <main className={styles.main}>
